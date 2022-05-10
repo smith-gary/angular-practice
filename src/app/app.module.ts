609ee06b69c4bf6ac7e06f72879e196ba08f04aa
Gary S <gsmith@appddictionstudio.com>
2022-05-10 13:13:45 -0500

    completed adding services and dependency injection

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ToastrService } from './common/toastr.service';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventThumbnailComponent } from './events/events-thumbnail.component';
 import { EventsListComponent } from './events/events-list.components';
@@ -5,17 +6,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule],
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbnailComponent,
-    NavBarComponent
+    NavBarComponent,
   ],
-  bootstrap: [EventsAppComponent]
+  providers: [EventService, ToastrService],
+  bootstrap: [EventsAppComponent],
 })
-export class AppModule { }
+export class AppModule {}
